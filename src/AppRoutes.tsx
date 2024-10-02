a377f0695f2be01c8226c9a621594d4e905d8462
Astroboy Himanshu <hhimanshu_be20@thapar.edu>
2024-10-03 01:09:41 +0530

    Added Lazy loading and support for fallback for components for improving performance

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,17 +1,20 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import SipPlanner from "./components/SipPlanner";
-import InvestmentPlanner from "./components/InvestmentPlanner";
+const HomePage = lazy(() => import("./components/HomePage"));
+const SipPlanner = lazy(() => import("./components/SipPlanner"));
+const InvestmentPlanner = lazy(() => import("./components/InvestmentPlanner"));
 
 function AppRoutes() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/sip-planner" element={<SipPlanner />} />
-          <Route path="/investment-planner" element={<InvestmentPlanner />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sip-planner" element={<SipPlanner />} />
+            <Route path="/investment-planner" element={<InvestmentPlanner />} />
+          </Routes>
+        </Suspense>
       </Router>
     </>
   );
