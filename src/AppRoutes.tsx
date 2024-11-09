4ad67233a61ccc62033a7381f8785a4f05e3360a
Astroboy Himanshu <hhimanshu_be20@thapar.edu>
2024-11-10 01:11:13 +0530

    refactored code & added pdf renderer library

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,9 +1,9 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
-import InvestmentDetails from "./components/investmentOptions/InvestmentDetails";
+import InvestmentDetails from "./components/investment-options/InvestmentDetails";
 const HomePage = lazy(() => import("./components/HomePage"));
-const SipPlanner = lazy(() => import("./components/SipPlanner"));
+const SipPlanner = lazy(() => import("./components/sip-calculator/SipPlanner"));
 const InvestmentPlanner = lazy(() => import("./components/InvestmentPlanner"));
 
 function AppRoutes() {
