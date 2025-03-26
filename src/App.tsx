a80ce19d383f83feaa0ba946c737f723e7e4d7c3
Astroboy Himanshu <hhimanshu_be20@thapar.edu>
2025-03-27 00:28:34 +0530

    add google analytics-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import ReactGA from "react-ga4";
+
+const MEASUREMENT_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_TRACKING_ID;
+
+ReactGA.initialize(MEASUREMENT_ID);
 
 function App() {
   return (
-    <>
+    <Router>
       <AppRoutes />
-    </>
+    </Router>
   );
 }
 
--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,36 +1,44 @@
-import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 import InvestmentDetails from "./components/investment-options/InvestmentDetails";
 import PlanGenerator from "./components/plan-generator/PlanGenerator";
+import ReactGA from "react-ga4";
+
 const HomePage = lazy(() => import("./components/home-page/HomePage"));
 const SipPlanner = lazy(() => import("./components/sip-calculator/SipPlanner"));
 const InvestmentPlanner = lazy(() => import("./components/InvestmentPlanner"));
 
 function AppRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname,
+      title: document.title,
+    });
+  }, [location]);
+
   return (
-    <>
-      <Router>
-        <Suspense
-          fallback={
-            <div className="flex items-center justify-center w-screen h-screen">
-              <BeatLoader />
-            </div>
-          }
-        >
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/sip-planner" element={<SipPlanner />} />
-            <Route path="/investment-options" element={<InvestmentPlanner />} />
-            <Route
-              path="/investment-planner/:type"
-              element={<InvestmentDetails />}
-            />
-            <Route path="/plan-generator" element={<PlanGenerator />} />
-          </Routes>
-        </Suspense>
-      </Router>
-    </>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center w-screen h-screen">
+          <BeatLoader />
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sip-planner" element={<SipPlanner />} />
+        <Route path="/investment-options" element={<InvestmentPlanner />} />
+        <Route
+          path="/investment-planner/:type"
+          element={<InvestmentDetails />}
+        />
+        <Route path="/plan-generator" element={<PlanGenerator />} />
+      </Routes>
+    </Suspense>
   );
 }
 
