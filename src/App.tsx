912c6e8b856533c0d4a61fecc35842d5f794ca11
Astroboy Himanshu <hhimanshu_be20@thapar.edu>
2025-04-03 00:22:54 +0530

    check ga bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
-import ReactGA from "react-ga4";
-
-const MEASUREMENT_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_TRACKING_ID;
-
-ReactGA.initialize(MEASUREMENT_ID);
-
 function App() {
   return (
     <Router>
